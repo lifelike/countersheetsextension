259d3b611134d65eaa04ec8139a9e2b086fffefc
Pelle Nilsson <pelle@pelle-n.net>
2010-08-29 00:49:27 +0200

    Inkscape 0.48 has the user extensions in .config/inkscape rather than .inkscape. Detecting 0.48. This is not very portable, but works for me.

--- testrun.sh
+++ testrun.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-cp /usr/share/inkscape/extensions/simplepath.py ~/.inkscape/extensions/
-cp *.inx *.py ~/.inkscape/extensions/
+if [ -n "`inkscape --version|grep 0.48`" ]; then
+    EXTDIR=~/.config/inkscape/extensions/
+else
+    EXTDIR=~/.inkscape/extensions/
+fi
+
+cp /usr/share/inkscape/extensions/simplepath.py $EXTDIR
+cp *.inx *.py $EXTDIR
 inkscape $*
