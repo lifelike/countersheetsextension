c2fc38e2a2a5abc094728586dccdf12d4dbd059b
Pelle Nilsson <perni@lysator.liu.se>
2011-05-14 22:33:47 +0200

    Updated testrun script to at least copy the files correctly on Mac OS (although inkscape does not start anyway).

--- testrun.sh
+++ testrun.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [ -n "`inkscape --version|grep 0.48`" ]; then
+if [ -e ~/.config/inkscape/extensions/ ]; then
     EXTDIR=~/.config/inkscape/extensions/
 else
     EXTDIR=~/.inkscape/extensions/
 fi
 
-cp /usr/share/inkscape/extensions/simplepath.py $EXTDIR
 cp *.inx *.py $EXTDIR
+
 inkscape $*
