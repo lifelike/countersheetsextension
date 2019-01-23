704a2f700fe46152eb0d6c417fa8517a4a27a5c5
Pelle Nilsson <pelle@hexandcounter.org>
2019-01-23 22:59:23 +0100

    Copyright 2019

--- countersheet.py
+++ countersheet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 
-# Copyright 2008-2018 Pelle Nilsson and contributors
+# Copyright 2008-2019 Pelle Nilsson and contributors
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
